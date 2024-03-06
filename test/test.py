1a66274ff8288b632053bb1b566ef85fc8074e06
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-03-06 15:07:03 +0300

    add datframefeature

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from app.sequant_tools import SequantTools
 
-max_peptide_length = 84
+
 polymer_type = 'DNA'
 seq_list = ['Atgc', 'GC']
 seq_df = pd.DataFrame()
@@ -9,8 +9,8 @@ seq_df = pd.DataFrame()
 sqt = SequantTools(
     sequences=seq_list,
     polymer_type=polymer_type,
-    max_sequence_length=max_peptide_length,
-    model_folder_path=r'app/utils/models/nucleic_acids'
+    model_folder_path=r'../app/utils/models/proteins',
+    add_peptide_descriptors=True
 )
 X_ref = sqt.generate_latent_representations()
 print(X_ref)
