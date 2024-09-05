bbccb131a9b9ca39a57de40713b1fe9bf86ac724
Evgeniy Nam <ew.nam@yandex.ru>
2024-09-05 14:55:02 +0300

    split classification task on subtasks

--- app/becnhmarking/classification_nonfouling.py
+++ app/becnhmarking/classification_nonfouling.py
@@ -18,12 +18,6 @@ print("imports done")
 random_state = 2024
 
 # Data import
-hemo_neg_df = pd.read_csv('/nfs/home/enam/SeQuant/app/utils/data/hemo_neg.csv')
-hemo_neg_df['label'] = 0
-
-hemo_pos_df = pd.read_csv('/nfs/home/enam/SeQuant/app/utils/data/hemo_pos.csv')
-hemo_pos_df['label'] = 1
-
 nf_neg_df = pd.read_csv('/nfs/home/enam/SeQuant/app/utils/data/nf_neg.csv')
 nf_neg_df['label'] = 0
 
@@ -31,10 +25,7 @@ nf_pos_df = pd.read_csv('/nfs/home/enam/SeQuant/app/utils/data/nf_pos.csv')
 nf_pos_df['label'] = 1
 
 # Data preprocessing
-hemo_df = pd.concat([hemo_neg_df, hemo_pos_df], ignore_index=True)
 nf_df = pd.concat([nf_neg_df, nf_pos_df], ignore_index=True)
-
-hemo_df = hemo_df.sample(frac=1, random_state=random_state).reset_index(drop=True)
 nf_df = nf_df.sample(frac=1, random_state=random_state).reset_index(drop=True)
 
 print("Data has been imported")
@@ -54,23 +45,12 @@ def data_filtration(data):
     return filtered_data
 
 
-hemo_df_filtred = data_filtration(hemo_df)
 nf_df_filtred = data_filtration(nf_df)
 
 # Retrieving descriptors by using SeQuant
-sequences_hemo = hemo_df_filtred['seq']
 sequences_nf = nf_df_filtred['seq']
 max_peptide_length = 96
 polymer_type = 'protein'
-
-sqt_hemo = SequantTools(
-    sequences=sequences_hemo,
-    polymer_type=polymer_type,
-    max_sequence_length=max_peptide_length,
-    encoding_strategy='protein'
-)
-
-descriptors_hemo = sqt_hemo.generate_latent_representations()
 
 sqt_nf = SequantTools(
     sequences=sequences_nf,
@@ -80,22 +60,21 @@ sqt_nf = SequantTools(
 )
 
 descriptors_nf = sqt_nf.generate_latent_representations()
+descriptors_nf.to_csv('/nfs/home/enam/SeQuant/app/utils/data/descriptors_nf.csv', index=False)
+del descriptors_nf
 
 print("Descriptors have been received")
 print("\n")
 
 # Predictions
-targets_hemo = hemo_df_filtred['label']
 targets_nf = nf_df_filtred['label']
+descriptor_nf = pd.read_csv('/nfs/home/enam/SeQuant/app/utils/data/descriptors_nf.csv')
+print('Descriptors have been read')
+print("\n")
 
 # Stratified split
-X_train_val_hemo, X_test_hemo, y_train_val_hemo, y_test_hemo = train_test_split(
-    descriptors_hemo, targets_hemo, test_size=0.1, stratify=targets_hemo, random_state=random_state)
-X_train_hemo, X_val_hemo, y_train_hemo, y_val_hemo = train_test_split(
-    X_train_val_hemo, y_train_val_hemo, test_size=0.1, stratify=y_train_val_hemo, random_state=random_state)
-
 X_train_val_nf, X_test_nf, y_train_val_nf, y_test_nf = train_test_split(
-    descriptors_nf, targets_nf, test_size=0.1, stratify=targets_nf, random_state=random_state)
+    descriptor_nf, targets_nf, test_size=0.1, stratify=targets_nf, random_state=random_state)
 X_train_nf, X_val_nf, y_train_nf, y_val_nf = train_test_split(
     X_train_val_nf, y_train_val_nf, test_size=0.1, stratify=y_train_val_nf, random_state=random_state)
 
@@ -136,7 +115,7 @@ param_grids = {
 
 
 def optimize_model_with_grid_search(model_name, model, X_train, X_val, y_train, y_val):
-    grid_search = GridSearchCV(model, param_grids[model_name], cv=10, scoring='accuracy')
+    grid_search = GridSearchCV(model, param_grids[model_name], cv=5, scoring='accuracy', n_jobs=-1)
     grid_search.fit(X_train, y_train)
 
     best_model = grid_search.best_estimator_
@@ -180,50 +159,17 @@ def evaluate_model(model, X_train, X_test, y_train, y_test):
     }
 
 
-print('Optimization of HEMO dataset started')
-print("\n")
-
-# Optimization with grid search on hemo dataset
-results_hemo = {}
-best_models_hemo = {}
-for name, model in models.items():
-    results_hemo[name], best_models_hemo[name] = optimize_model_with_grid_search(name, model, X_train_hemo, X_val_hemo,
-                                                                                 y_train_hemo, y_val_hemo)
-
-# Results for hemo dataset
-print('HEMOLYSIS DATASET')
-for name, metrics in results_hemo.items():
-    print(f"Results for {name}:")
-    for metric_name, value in metrics.items():
-        if value is not None:
-            print(f"{metric_name}: {value}")
-    print("\n")
-
-print('Evaluation of HEMO ds')
-print("\n")
-# Final evaluation on test set using the best models
-final_results_hemo = {}
-for name, model in best_models_hemo.items():
-    final_results_hemo[name] = evaluate_model(model, X_train_hemo + X_val_hemo, X_test_hemo, y_train_hemo + y_val_hemo,
-                                              y_test_hemo)
-
-# Print final results on test set
-print('FINAL EVALUATION ON TEST SET (HEMOLYSIS DATASET)')
-for name, metrics in final_results_hemo.items():
-    print(f"Results for {name}:")
-    for metric_name, value in metrics.items():
-        if value is not None:
-            print(f"{metric_name}: {value:.4f}")
-    print("\n")
-
 print('Optimization of NF ds')
 print("\n")
 # Optimization with grid search on nonfouling dataset
 results_nf = {}
 best_models_nf = {}
+counter = 1
 for name, model in models.items():
     results_nf[name], best_models_nf[name] = optimize_model_with_grid_search(name, model, X_train_nf, X_val_nf,
                                                                              y_train_nf, y_val_nf)
+    print(f'model {counter} passed')
+    counter += 1
 
 # Results for nonfouling dataset
 print('NONFOULING DATASET')
