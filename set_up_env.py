4ae7075076d1984769949a88d29741e1578c653c
Evgeniy Nam <ew.nam@yandex.ru>
2024-11-23 18:11:36 +0300

    modify setup file

--- set_up_env.py
+++ set_up_env.py
@@ -1,11 +1,9 @@
 import os
 PROJECT_PATH = os.getcwd()
-NUCLEIC_ACIDS_PATH = os.path.join(PROJECT_PATH, 'app/utils/models/nucleic_acids/')
 PROTEINS_PATH = os.path.join(PROJECT_PATH, 'app/utils/models/proteins/')
 
 if __name__ == "__main__":
     envs = f'{PROJECT_PATH=}\n' \
-           f'{NUCLEIC_ACIDS_PATH=}\n' \
            f'{PROTEINS_PATH=}\n'
 
     with open('.env', 'w') as f:
